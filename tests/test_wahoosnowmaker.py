08a9cc98972b9870bc913fc290b2adc02bda2366
Thomas Camminady <0milieux_member@icloud.com>
2023-04-24 16:12:15 +0200

    Initial commit after initializing the project.

---
+++ tests/test_wahoosnowmaker.py
@@ -0,0 +1,8 @@
+"""Tests for `wahoosnowmaker` package."""
+
+
+from wahoosnowmaker import __version__
+
+
+def test_version() -> None:
+    assert __version__ == "0.1.0"
---
+++ wahoosnowmaker/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for wahoosnowmaker."""
+from wahoosnowmaker.utils.config import config
+from wahoosnowmaker.utils.logger import logger
+
+__all__ = ["config", "logger"]
+__author__ = """Thomas Camminady"""
+__email__ = "0milieux_member@icloud.com"
+__version__ = "0.1.0"
